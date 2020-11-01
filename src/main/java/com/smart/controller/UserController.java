33eb83a8ab98b434ea8b1c9e3667f3cd3250bec0
ajit51 <ajitk433@gmail.com>
2020-11-02 00:34:05 +0530

    file upload

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,17 +1,31 @@
 package com.smart.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.security.Principal;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
+import com.smart.healper.Message;
 
 @Controller
 @RequestMapping("/user")
@@ -40,6 +54,42 @@ public class UserController {
 
 		model.addAttribute("title", "Add Contact");
 		model.addAttribute("contact", new Contact());
+
+		return "normal/add_contact_from";
+	}
+
+	@PostMapping("/process-contact")
+	public String process_Contact(@ModelAttribute Contact contact, @RequestParam("profileImage") MultipartFile file,
+			Principal principal, HttpSession session) {
+
+		try {
+			String name = principal.getName();
+
+			System.out.println("file Name " + file.getOriginalFilename());
+			System.out.println("type : " + file.getContentType());
+
+			if (file.isEmpty()) {
+				System.out.println("file is empty");
+			} else {
+
+				contact.setImage(file.getOriginalFilename());
+				File saveFile = new ClassPathResource("static/img").getFile();
+
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+			}
+
+			User user = userRepository.getUserByUserName(name);
+			user.getContacts().add(contact);
+			contact.setUser(user);
+			userRepository.save(user);
+			System.out.println("contact " + contact);
+			session.setAttribute("message", new Message("Your contact is added !! Add more..", "success"));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.setAttribute("message", new Message("Some went wrong !! Ttry again", "danger"));
+		}
 
 		return "normal/add_contact_from";
 	}
